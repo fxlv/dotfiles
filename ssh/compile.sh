5fb0476e1d1e75533eccfbb619119ab2f22fb90a
Kaspars Mickevics <kaspars@fx.lv>
2015-02-12 20:54:47 +0200

    Decrypting hosts files is optional now
    Degrade gracefully if some hosts files cannot be decrypted

--- ssh/compile.sh
+++ ssh/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cat default_config > ssh_config
-cat *.hosts >> ssh_config
+for hostsfile in *.hosts
+do  
+    if file $hostsfile|grep -q ASCII; then
+        echo "Adding $hostsfile to ssh_config"
+        cat $hostsfile >> ssh_config
+    else
+        echo "Skipping non-decrypted file $hostsfile"
+    fi
+done
