619f0244d2ec6020fb909f545e45531f2a87995e
Kaspars Mickevics <kaspars@fx.lv>
2015-02-12 19:46:52 +0200

    SSH config updated to use git-crypt

---
+++ ssh/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cat default_config > ssh_config
+cat *.hosts >> ssh_config
--- ssh/encrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# encrypt with 'secrets' pub key 
-if gpg --default-recipient secrets -e config; then
-    rm -v config
-else
-    echo "Encryption somehow failed"
-fi
