34fa35557de4adde751eaab48d7a02dff5ce648e
Kaspars Mickevics <kaspars@fx.lv>
2019-03-10 11:32:09 +0000

    check all .sh files

--- ssh/compile.sh
+++ ssh/compile.sh
@@ -3,9 +3,9 @@
 cat default_config > ssh_config
 for hostsfile in *.hosts
 do  
-    if file $hostsfile|grep -q ASCII; then
+    if file "$hostsfile"|grep -q ASCII; then
         echo "Adding $hostsfile to ssh_config"
-        cat $hostsfile >> ssh_config
+        cat "$hostsfile" >> ssh_config
         chmod 640 ssh_config
     else
         echo "Skipping non-decrypted file $hostsfile"
